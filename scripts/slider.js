05f0570393d0817b77659b052d605453c8d65d82
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-02-28 14:30:23 +0300

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,33 +1,17 @@
-let startX, movedX;
-let currentIndex = 0; // Текущий индекс слайда
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length; // Общее количество слайдов
+const swiper = new Swiper('.swiper-container', {
+  // Настройки Swiper
+  direction: 'horizontal',
+  loop: false,
 
-slider.addEventListener('touchstart', (e) => {
-  startX = e.touches[0].clientX;
-  movedX = 0; // Сбросить смещение при начале касания
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Включение управления свайпами
+  simulateTouch: true,
+  touchRatio: 1,
+  touchAngle: 45,
+  grabCursor: true,
+  slideToClickedSlide: true,
 });
-
-slider.addEventListener('touchmove', (e) => {
-  const touchMoveX = e.touches[0].clientX;
-  movedX = startX - touchMoveX;
-});
-
-slider.addEventListener('touchend', () => {
-  // Проверяем, было ли движение достаточно значительным, чтобы считаться свайпом
-  if (Math.abs(movedX) > 50) { // Свайп на 50px
-    if (movedX > 0 && currentIndex < totalSlides - 1) {
-      // Свайп влево и проверка, что это не последний слайд
-      currentIndex++;
-      moveSlider(-currentIndex * slider.offsetWidth);
-    } else if (movedX < 0 && currentIndex > 0) {
-      // Свайп вправо и проверка, что это не первый слайд
-      currentIndex--;
-      moveSlider(-currentIndex * slider.offsetWidth);
-    }
-  }
-});
-
-function moveSlider(offset) {
-  slider.style.transform = `translateX(${offset}px)`;
-}
