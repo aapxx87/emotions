37b106c15b819a5770dafa16125cccf4dcf21be8
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-02-28 14:16:51 +0300

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,35 +1,33 @@
-let startX;
-const slider = document.getElementById('slider');
+let startX, movedX;
+let currentIndex = 0; // Текущий индекс слайда
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length; // Общее количество слайдов
 
 slider.addEventListener('touchstart', (e) => {
   startX = e.touches[0].clientX;
+  movedX = 0; // Сбросить смещение при начале касания
 });
 
 slider.addEventListener('touchmove', (e) => {
   const touchMoveX = e.touches[0].clientX;
-  const diff = startX - touchMoveX;
-  if (Math.abs(diff) > 50) { // Свайп на 50px
-    if (diff > 0) {
-      // Свайп влево
-      moveSlider('left');
-    } else {
-      // Свайп вправо
-      moveSlider('right');
+  movedX = startX - touchMoveX;
+});
+
+slider.addEventListener('touchend', () => {
+  // Проверяем, было ли движение достаточно значительным, чтобы считаться свайпом
+  if (Math.abs(movedX) > 50) { // Свайп на 50px
+    if (movedX > 0 && currentIndex < totalSlides - 1) {
+      // Свайп влево и проверка, что это не последний слайд
+      currentIndex++;
+      moveSlider(-currentIndex * slider.offsetWidth);
+    } else if (movedX < 0 && currentIndex > 0) {
+      // Свайп вправо и проверка, что это не первый слайд
+      currentIndex--;
+      moveSlider(-currentIndex * slider.offsetWidth);
     }
-    startX = touchMoveX; // Сбросить начальную точку для следующего движения
   }
 });
 
-function moveSlider(direction) {
-  const slides = document.querySelectorAll('.slide');
-  const sliderWidth = slider.offsetWidth;
-  const currentTransform = getComputedStyle(slider).transform !== 'none'
-    ? parseInt(getComputedStyle(slider).transform.split(',')[4].trim())
-    : 0;
-
-  if (direction === 'left' && Math.abs(currentTransform) < sliderWidth * (slides.length - 1)) {
-    slider.style.transform = `translateX(${currentTransform - sliderWidth}px)`;
-  } else if (direction === 'right' && currentTransform < 0) {
-    slider.style.transform = `translateX(${currentTransform + sliderWidth}px)`;
-  }
+function moveSlider(offset) {
+  slider.style.transform = `translateX(${offset}px)`;
 }
